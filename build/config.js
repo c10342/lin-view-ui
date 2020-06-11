afa73009e50d8c068b3f4c75fe6f34acd96c3f20
c10342 <you@example.com>
2020-06-11 22:54:49 +0800

    feat：添加组件文档上传脚本

---
+++ build/config.js
@@ -0,0 +1,7 @@
+const path = require("path");
+module.exports = {
+  accessKey: "YBEYWaqwXhGNpNIuEOsuSyLtQ0i0b34gobjSYsKL",
+  secretKey: "3BCpdcLZZ3wXw-l5psmu-D8RHjnmmlJnciUTeVuK",
+  bucket: "vue-ui",
+  basePath: path.join(__dirname, "../docs-dist"), //打包目录
+};
