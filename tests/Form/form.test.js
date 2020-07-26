e49412defd4661c624deda1c9874703f217f84a4
c10342 <you@example.com>
2020-07-26 14:59:52 +0800

    feat：添加Form组件单元测试

--- tests/Form/form.test.js
+++ tests/Form/form.test.js
@@ -2,8 +2,18 @@ import { shallowMount } from "@vue/test-utils";
 
 import Form from "packages/Form";
 
+const creatForm = (obj = {}) => {
+  return shallowMount(Form, obj);
+};
+
 describe("属性", () => {
-  it("type", () => {
-    // const wrapper = shallowMount(Form);
+  it("labelWidth", () => {
+    const wrapper = creatForm({ propsData: { labelWidth: "20px", model: {} } });
+    expect(wrapper.vm.labelWidth).toEqual("20px");
+  });
+  it("model", () => {
+    const model = { name: "张三", age: 14 };
+    const wrapper = creatForm({ propsData: { model } });
+    expect(wrapper.vm.model).toEqual(model);
   });
 });
