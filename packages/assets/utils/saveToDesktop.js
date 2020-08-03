097a6f118d49c1b468dfacef105ff2b9612b7221
c10342 <you@example.com>
2020-08-03 22:06:00 +0800

    feat：添加保存网页到桌面的方法

---
+++ packages/assets/utils/saveToDesktop.js
@@ -0,0 +1,19 @@
+/**
+ * 保存到桌面
+ * @param {string} sName 保存到桌面的名称
+ * @param {string} sUrl 保存的网址
+ */
+const saveToDesktop = (sName, sUrl) => {
+  try {
+    const WshShell = new ActiveXObject("WScript.Shell");
+    const oUrlLink = WshShell.CreateShortcut(
+      WshShell.SpecialFolders("Desktop") + "\\" + sName + ".url"
+    );
+    oUrlLink.TargetPath = sUrl;
+    oUrlLink.Save();
+  } catch (e) {
+    throw new SyntaxError("当前IE安全级别不允许操作！");
+  }
+};
+
+export default saveToDesktop;
