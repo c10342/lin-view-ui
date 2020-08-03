f9cf6721b629a296c88468a806223b82871df97c
c10342 <you@example.com>
2020-08-03 22:09:48 +0800

    feat：使用require.context自动引入utils目录下的所有js文件并导出

--- packages/assets/utils/index.js
+++ packages/assets/utils/index.js
@@ -1,5 +1,13 @@
-import getScroll from "./getScroll.js";
+const exportObj = {};
+const testComps = require.context("./", false, /\.js$/);
 
-export default {
-  getScroll,
-};
+testComps.keys().forEach((key) => {
+  const fileName = key.replace(/(.*\/)*([^.]+).*/gi, "$2");
+  if (fileName === "index") {
+    return;
+  }
+  const componentEntity = testComps(key).default;
+  exportObj[fileName] = componentEntity;
+});
+
+export default exportObj;
