d181967e9b039db7cd5669b1a3aa4d0fda30547a
c10342 <you@example.com>
2020-08-17 22:22:13 +0800

    feat：添加获取浏览器视口的宽高的方法

---
+++ src/utils/getWindowHeightAndWidth.js
@@ -0,0 +1,17 @@
+/**
+ * 浏览器视口的宽高
+ */
+function getWindowHeightAndWidth() {
+  let windowHeight = 0;
+  let windowWidth = 0;
+  if (document.compatMode === "CSS1Compat") {
+    windowHeight = document.documentElement.clientHeight;
+    windowWidth = document.documentElement.clientWidth;
+  } else {
+    windowHeight = document.body.clientHeight;
+    windowWidth = document.body.clientWidth;
+  }
+  return { windowHeight, windowWidth };
+}
+
+export default getWindowHeightAndWidth;
