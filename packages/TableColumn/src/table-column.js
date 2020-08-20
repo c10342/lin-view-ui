68023b1237e5f8cb4c68826b4f1e03703fcb9328
c10342 <you@example.com>
2020-08-20 23:09:18 +0800

    feat：初始化TableColumn组件

---
+++ packages/TableColumn/src/table-column.js
@@ -0,0 +1,5 @@
+import TableColumn from "./table-column.vue";
+
+import "./style.scss";
+
+export default TableColumn;
---
+++ packages/TableColumn/src/table-column.vue
@@ -0,0 +1,28 @@
+<template>
+  <td>rowData</td>
+</template>
+
+<script>
+export default {
+  name: "LinTableColumn",
+  props: {
+    prop: {
+      type: String,
+    },
+    label: {
+      type: String,
+      require: true,
+    },
+    type: {
+      type: String,
+    },
+    rowData: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  mounted() {
+    console.log(this.rowData);
+  },
+};
+</script>
