bbddd2a29646d605bd8271d4e7f0980109caf1ed
c10342 <1376023740@qq.com>
2021-01-04 20:51:01 +0800

    feat:添加stylelint检查css样式

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["stylelint-config-standard","stylelint-config-recommended-scss", "stylelint-config-recess-order"],
+  plugins: ["stylelint-scss"],
+  rules: {
+    "no-descending-specificity": null,
+    'no-empty-source':null
+  }
+};
