c0123f9eb5feb39edc0c4c52031bbe278d5c95d3
c10342 <1376023740@qq.com>
2021-01-20 22:33:58 +0800

    feat(findparents.js): 添加查找父组件方法

---
+++ src/utils/findParents.js
@@ -0,0 +1,13 @@
+export default function findParents (context, componentName) {
+  const parents = [];
+  const parent = context.$parent;
+
+  if (parent) {
+    if (parent.$options.name === componentName) {
+      parents.push(parent);
+    }
+    return parents.concat(findParents(parent, componentName));
+  } else {
+    return [];
+  }
+}
