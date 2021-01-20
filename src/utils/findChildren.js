bb620f0d3b88d5962b5cb49d5e2dbc7861ae095f
c10342 <1376023740@qq.com>
2021-01-20 22:34:36 +0800

    feat(findchildren.js): 添加查找子组件方法

---
+++ src/utils/findChildren.js
@@ -0,0 +1,9 @@
+export default function findChildren (context, componentName) {
+  return context.$children.reduce((components, child) => {
+    if (child.$options.name === componentName) {
+      components.push(child);
+    }
+    const children = findChildren(child, componentName);
+    return components.concat(children);
+  }, []);
+}
