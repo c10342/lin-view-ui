54697e514257df0802cf380ca3cf493b4aaac93d
c10342 <1376023740@qq.com>
2021-02-28 00:49:06 +0800

    feat(button.d.ts): button组件添加类型声明文件

---
+++ types/Button.d.ts
@@ -0,0 +1,43 @@
+import { VNode } from "vue";
+import { LinViewUIComponent } from "./component";
+
+export type ButtonType =
+  | "default"
+  | "primary"
+  | "success"
+  | "warning"
+  | "danger"
+  | "info";
+
+export type ButtonSize = "default" | "medium" | "small";
+
+export declare class LinButton extends LinViewUIComponent {
+  //   类型
+  type: ButtonType;
+  //   是否为朴素按钮
+  plain: boolean;
+  //   是否为圆角按钮
+  round: boolean;
+  //   是否为圆形按钮
+  circle: boolean;
+  //   是否禁用
+  disabled: boolean;
+  //   按钮图标
+  icon: string;
+  //   尺寸
+  size: ButtonSize;
+  //   是否加载中状态
+  loading: boolean;
+  //   加载器颜色
+  loadingColor: string;
+  //   加载器大小
+  loadingSize: string;
+
+  // 点击按钮时触发
+  $emit(eventName: "click"): this;
+
+  $slots: {
+    "": VNode[];
+    default: VNode[];
+  };
+}
