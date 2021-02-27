f1ebe83810610882f7d623e78f344decb95eddc7
c10342 <1376023740@qq.com>
2021-02-27 15:38:42 +0800

    feat(badge.d.ts): 添加Badge组件类型声明文件

---
+++ types/Badge.d.ts
@@ -0,0 +1,20 @@
+import { VNode } from "vue";
+import { LinViewUIComponent } from "./component";
+
+export type BadgeType = "primary" | "success" | "warning" | "danger" | "info";
+
+export declare class Badge extends LinViewUIComponent {
+  // 显示的值
+  value: string | number;
+  // 最大值，超过最大值会显示 '{max}+'
+  max: number;
+  // 是否显示小圆点
+  isDot: boolean;
+  // 类型
+  type: BadgeType;
+  // 插槽
+  $slots: {
+    "": VNode[];
+    default: VNode[];
+  };
+}
