6d4535d33e015f38ea24db5a2591e2187d4c4cfb
c10342 <1376023740@qq.com>
2021-02-28 21:00:22 +0800

    feat(component): 补充DateMixin混入对象类型声明

--- types/component.d.ts
+++ types/component.d.ts
@@ -1,5 +1,20 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-export declare class LinViewUIComponent extends Vue{
-    static install (vue:typeof Vue):void
+export declare class LinViewUIComponent extends Vue {
+  static install(vue: typeof Vue): void;
+}
+
+export type DateMixinDateType = Date | string | number;
+
+export interface LinViewUIDateMixin {
+  // 是否禁用
+  disabled: boolean;
+  // 禁用小于等于该日期的日期
+  disabledBeforeDate: DateMixinDateType;
+  // 禁用大于等于该日期的日期
+  disabledAfterDate: DateMixinDateType;
+  // 禁用指定范围内的日期
+  disabledRangeDate: Array<DateMixinDateType>;
+  // 禁用指定日期
+  disabledDate: Array<DateMixinDateType>;
 }
