8a545b6ad3e1ba842851f974cf0a8088f6673f68
c10342 <1376023740@qq.com>
2021-02-28 20:33:30 +0800

    style(collapse-group): collapse-group组件添加注释说明

--- packages/CollapseGroup/src/collapse-group.vue
+++ packages/CollapseGroup/src/collapse-group.vue
@@ -10,13 +10,16 @@ import { cloneDeep } from 'lodash';
 export default {
   name: 'LinCollapseGroup',
   props: {
+    // 当前激活面板的name，accordion为true就是String或者Number,否则就是Array
     value: {
-      type: [Array, String]
+      type: [Array, String, Number]
     },
+    // 是否开启手风琴模式，开启后每次至多展开一个面板
     accordion: {
       type: Boolean,
       default: false
     },
+    // 是否开启简洁模式
     simple: {
       type: Boolean,
       default: false
@@ -25,13 +28,13 @@ export default {
   computed: {
     collapseValue: {
       get () {
-        if (typeof this.value === 'string' || Array.isArray(this.value)) {
+        if (this.canUseValue()) {
           return this.value;
         }
         return this.valueData;
       },
       set (val) {
-        if (typeof this.value === 'string' || Array.isArray(this.value)) {
+        if (this.canUseValue()) {
           this.$emit('input', this.handleData(val));
         } else {
           this.valueData = this.handleData(val);
@@ -47,22 +50,31 @@ export default {
   },
   data () {
     return {
+      // 外部没有绑定value的时候需要使用内部的valueData，防止因为外部没有绑定value而不能使用
       valueData: ''
     };
   },
   methods: {
+    // 根据是否为手风琴模式返回一个(String|Number)或者Array
     handleData (data) {
       let da;
       if (this.accordion) {
+        // 手风琴模式，一个值
         if (data.length > 0) {
           da = data[0];
         } else {
           da = '';
         }
       } else {
+        // 非手风琴，数组
         da = data;
       }
       return cloneDeep(da);
+    },
+    // 是否使用外部传来value值
+    canUseValue () {
+      const value = this.value;
+      return typeof value === 'string' || Array.isArray(value) || typeof value === 'number';
     }
   }
 };
