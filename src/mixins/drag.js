4676446b0655e04d323e3148ba41be96743d05fc
c10342 <1376023740@qq.com>
2021-03-01 19:54:03 +0800

    style(drag.js): 混入对象DragMixin添加注释说明

--- src/mixins/drag.js
+++ src/mixins/drag.js
@@ -1,5 +1,6 @@
 const drag = {
   props: {
+    // 是否可进行拖拽
     drag: {
       type: Boolean,
       default: false
@@ -7,6 +8,7 @@ const drag = {
   },
   data () {
     return {
+      // 拖拽的时候的样式，就是位置
       dialogStyle: {
         top: '0',
         left: '50%',
@@ -15,6 +17,7 @@ const drag = {
     };
   },
   mounted () {
+    // 开始点
     this.startY = 0;
     this.startX = 0;
   },
@@ -22,21 +25,27 @@ const drag = {
     this.removeListener();
   },
   methods: {
+    // 鼠标按下，开始拖拽
     onDragClick (event) {
       if (!this.drag) {
         return;
       }
+      // 防止选中文字，导致mouseup事件丢失
       document.body.classList.add('user-select-none');
+      // 开始点
       this.startY = event.clientY;
       this.startX = event.clientX;
       document.addEventListener('mousemove', this.onMousemove);
       document.addEventListener('mouseup', this.onMouseup);
     },
     onMousemove (event) {
+      // 结束点
       const endY = event.clientY;
       const endX = event.clientX;
+      // 移动的偏移量
       const offsetY = endY - this.startY;
       const offsetX = endX - this.startX;
+      // 需要拖拽容器
       const dialog = this.$refs.dragContainer;
       const rect = dialog.getBoundingClientRect();
       const left = rect.left;
@@ -49,6 +58,7 @@ const drag = {
       this.startX = endX;
       let dialogTop = parseInt(top + offsetY, 10);
       let dialogLeft = parseInt(left + offsetX, 10);
+      // 判断是否越界，超出视图范围
       dialogTop = dialogTop < 0 ? 0 : dialogTop;
       dialogLeft = dialogLeft < 0 ? 0 : dialogLeft;
       dialogTop = dialogTop > windowHeight - height ? windowHeight - height : dialogTop;
@@ -63,6 +73,7 @@ const drag = {
         marginRight: 0
       };
     },
+    // 鼠标抬起
     onMouseup () {
       document.body.classList.remove('user-select-none');
       this.removeListener();
