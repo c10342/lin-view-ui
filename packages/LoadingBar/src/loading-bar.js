8e507ab2452f94ebe8e558e207bd21fb1518aa32
c10342 <1376023740@qq.com>
2021-03-05 18:58:43 +0800

    style(loading-bar): loading-bar组件添加注释说明

--- packages/LoadingBar/src/loading-bar.js
+++ packages/LoadingBar/src/loading-bar.js
@@ -10,6 +10,7 @@ let removeTimer = null;
 
 const LLoadingBar = () => new LoadingBarConstructor();
 
+// 设置全局配置信息
 LoadingBarConstructor.prototype.config = function config (options) {
   Object.keys(options).forEach((key) => {
     if (key === 'isError' || key === 'totalProgress') {
@@ -19,6 +20,7 @@ LoadingBarConstructor.prototype.config = function config (options) {
   });
 };
 
+// 初始化加载进度条
 LoadingBarConstructor.prototype.init = function init () {
   clearTimeout(timer);
   this.totalProgress = 0;
@@ -28,21 +30,25 @@ LoadingBarConstructor.prototype.init = function init () {
   return this;
 };
 
+// 显示加载进度条
 LoadingBarConstructor.prototype.start = function start () {
   this.init();
 
   timer = setInterval(() => {
+    // 小于90的时候才进行前进
     if (this.totalProgress < 90) {
       this.totalProgress += (this.percentNum || Math.random()) * this.speed;
     }
   }, 100);
 };
 
+// 关闭加载进度条
 LoadingBarConstructor.prototype.end = function end () {
   // timer || this.init();
   if (!timer) {
     this.init();
   }
+  // 先把总进度设置为100，让他走完
   this.totalProgress = 100;
   clearTimeout(removeTimer);
   removeTimer = setTimeout(() => {
@@ -52,6 +58,7 @@ LoadingBarConstructor.prototype.end = function end () {
   }, 200);
 };
 
+// 显示错误进度条
 LoadingBarConstructor.prototype.error = function error () {
   this.end();
   this.totalProgress = 100;
--- packages/LoadingBar/src/loading-bar.vue
+++ packages/LoadingBar/src/loading-bar.vue
@@ -22,11 +22,17 @@ export default {
   name: 'LinLoadingBar',
   data () {
     return {
+      // 加载器（转圈圈的那个东西），运动形式
       easing: 'linear',
+      // 是否为错误类型
       isError: false,
+      // 显示加载器
       showSpinner: true,
+      // 加载的总进度
       totalProgress: 0,
+      // 每次前进的百分比
       percentNum: 0,
+      // 加载速度
       speed: 5
     };
   }
