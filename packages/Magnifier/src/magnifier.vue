d7d0e0e5fa5d8704fcb869e6c6c8a36df59cecfe
c10342 <1376023740@qq.com>
2021-03-11 19:59:00 +0800

    style(magnifier): magnifier组件添加代码注释

--- packages/Magnifier/src/magnifier.vue
+++ packages/Magnifier/src/magnifier.vue
@@ -31,14 +31,17 @@
 export default {
   name: 'LinMagnifier',
   props: {
+    // 小图片路径地址
     smallPic: {
       type: String,
       require: true
     },
+    // 大图片路径地址
     bigPic: {
       type: String,
       require: true
     },
+    // 小图片样式（必须设置宽高，否则宽高为 0）
     smallStyle: {
       type: Object,
       default: () => ({
@@ -46,6 +49,7 @@ export default {
         height: 0
       })
     },
+    // 大图片样式（必须设置宽高，否则宽高为 0）
     bigStyle: {
       type: Object,
       default: () => ({
@@ -53,13 +57,18 @@ export default {
         height: 0
       })
     },
+    // 鼠标移动的遮罩层背景色
     maskColor: String
   },
   data () {
     return {
+      // 是否显示鼠标移动遮罩层
       showMoveMask: false,
+      // 是否显示大图片
       showBigImg: false,
+      // 鼠标移动的样式,就是鼠标遮罩层需要跟随鼠标移动
       moveStyle: { left: 0, top: 0 },
+      // 大图片样式
       bigImgStyle: { left: 0, top: 0 }
     };
   },
@@ -67,22 +76,26 @@ export default {
     bigBoxStyle () {
       return {
         ...this.bigStyle,
+        // 让大图片距离左边小图片的边距
         left: `${parseFloat(this.smallStyle.width) + 10}px`
       };
     }
   },
   mounted () {
+    // 初始化DOM
     this.smallBox = this.$refs.smallBox;
     this.moveMask = this.$refs.moveMask;
     this.bigBox = this.$refs.bigBox;
   },
   methods: {
     onMouseover () {
+      // 鼠标进来
       this.showMoveMask = true;
       this.showBigImg = true;
       this.$emit('show');
     },
     onMouseout () {
+      // 鼠标离开
       this.showMoveMask = false;
       this.showBigImg = false;
       this.$emit('hide');
@@ -91,22 +104,16 @@ export default {
       const { smallBox } = this;
       const { moveMask } = this;
       const { bigBox } = this;
+      // 计算出鼠标距离小图片左边的位置，同时要使鼠标遮罩层居中，就是鼠标要在遮罩层中间
       let x = event.clientX -
         smallBox.getBoundingClientRect().left -
         moveMask.offsetWidth / 2;
-
+        // 同上，计算上边位置
       let y = event.clientY -
         smallBox.getBoundingClientRect().top -
         moveMask.offsetHeight / 2;
-      // let x =
-      //   event.clientX -
-      //   smallBox.offsetParent.offsetLeft -
-      //   moveMask.offsetWidth / 2;
-      // let y =
-      //   event.clientY -
-      //   smallBox.offsetParent.offsetTop -
-      //   moveMask.offsetHeight / 2;
-      // document.getElementById().getBoundingClientRect
+
+      // 临界值处理
       if (x < 0) {
         x = 0;
       } else if (x > smallBox.offsetWidth - moveMask.offsetWidth) {
