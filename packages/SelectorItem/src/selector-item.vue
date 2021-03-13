af515decf82b8aa15fc4ba66dac364fd39e66ed4
c10342 <1376023740@qq.com>
2021-03-13 16:53:36 +0800

    style(selector-item): selector-item组件添加代码注释

--- packages/SelectorItem/src/selector-item.vue
+++ packages/SelectorItem/src/selector-item.vue
@@ -12,13 +12,16 @@
 export default {
   name: 'LinSelectorItem',
   props: {
+    // 标签内容
     label: {
       type: String,
       default: ''
     },
+    // 选项的值
     value: {
       default: ''
     },
+    // 是否禁用
     disabled: {
       type: Boolean,
       default: false
@@ -30,17 +33,21 @@ export default {
     }
   },
   computed: {
+    // 是否是选择中状态
     active () {
       if (this.group) {
         const { valueKey } = this.group;
         const { toString } = Object.prototype;
         if (toString.call(this.value) === '[object Object]' && valueKey) {
+          // 对象的情况
           return this.value[valueKey] === this.group.value[valueKey];
         }
+        // 其他情况
         return this.value === this.group.value;
       }
       return false;
     },
+    // 是否禁用该选项
     isDisabled () {
       if (this.group && this.group.disabled) {
         return true;
@@ -49,6 +56,7 @@ export default {
     }
   },
   methods: {
+    // 点击该项
     onClick () {
       if (this.group && !this.isDisabled) {
         const value = JSON.parse(JSON.stringify(this.value));
