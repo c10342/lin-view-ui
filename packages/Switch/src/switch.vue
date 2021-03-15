9fe9de816993de96cb38036076b340fe969eed7d
c10342 <1376023740@qq.com>
2021-03-15 19:57:40 +0800

    style(switch): switch组件添加代码注释

--- packages/Switch/src/switch.vue
+++ packages/Switch/src/switch.vue
@@ -17,22 +17,27 @@ import dispatch from 'src/utils/dispatch.js';
 export default {
   name: 'LinSwitch',
   props: {
+    // 绑定值
     value: {
       type: Boolean,
       default: false
     },
+    //  打开时的背景色
     activeColor: {
       type: String,
       default: ''
     },
+    // 关闭时的背景色
     inactiveColor: {
       type: String,
       default: ''
     },
+    // 原生属性 name
     name: {
       type: String,
       default: ''
     },
+    // 是否禁用
     disabled: {
       type: Boolean,
       default: false
