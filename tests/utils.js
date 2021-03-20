c4925400b6b29c232be72d51dd82a67e5655eacf
c10342 <1376023740@qq.com>
2021-03-20 16:03:15 +0800

    feat(utils.js): 新增通用工具类创建vue实例

---
+++ tests/utils.js
@@ -0,0 +1,22 @@
+
+import Vue from "vue/dist/vue";
+
+import LinViewUI from 'src/index.js';
+
+Vue.use(LinViewUI)
+
+const createElm = function() {
+  const elm = document.createElement('div');
+
+  // elm.id = 'app' + ++id;
+  document.body.appendChild(elm);
+
+  return elm;
+};
+
+export const createVue = function({template}){
+    return new Vue({
+      template
+    }).$mount(createElm())
+  }
+  
