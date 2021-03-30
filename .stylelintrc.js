4e95101bdeeb59406706466a8db10db5428bd4a1
c10342 <1376023740@qq.com>
2021-03-30 17:56:24 +0800

    chore(.stylelintrc.js): 修改stylelint的配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-  extends: ["stylelint-config-standard","stylelint-config-recommended-scss", "stylelint-config-recess-order"],
-  plugins: ["stylelint-scss"],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-recess-order',
+  ],
+  plugins: ['stylelint-scss'],
   rules: {
-    "no-descending-specificity": null,
-    'no-empty-source':null
-  }
+    'no-descending-specificity': null,
+    'no-empty-source': null,
+    'declaration-block-trailing-semicolon': null,
+  },
 };
