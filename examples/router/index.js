5cf892d8cbf405be7dfad31f79d556ded1cb30a6
c10342 <1376023740@qq.com>
2021-04-12 10:15:28 +0800

    docs(examples): 新增examples入口，用作开发测试

---
+++ examples/router/index.js
@@ -0,0 +1,25 @@
+import VueRouter from 'vue-router';
+
+import Vue from 'vue';
+
+Vue.use(VueRouter);
+
+const pages = require.context('../pages', false, /\.vue$/);
+
+const routes = [];
+
+pages.keys().forEach((key) => {
+  const fileName = key.replace(/(.*\/)*([^.]+).*/gi, '$2');
+  const routeItem = {
+    path: `/${fileName}`,
+    name: fileName,
+    component: pages(key).default,
+  };
+  routes.push(routeItem);
+});
+
+console.log(routes);
+
+const router = new VueRouter({ routes });
+
+export default router;
