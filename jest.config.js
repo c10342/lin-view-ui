d874b4beb6dde3c9784a9d2c9b333390aa0d6be6
c10342 <1376023740@qq.com>
2021-04-16 17:55:05 +0800

    style(jest.config.js): eslint,清除对象最后一项的尾随逗号

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     'js',
     'json',
     // 告诉 Jest 处理 `*.vue` 文件
-    'vue',
+    'vue'
   ],
   transform: {
     // 用 `vue-jest` 处理 `*.vue` 文件
@@ -13,16 +13,16 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
     '\\.(css|scss)$': '<rootDir>/tests/__mocks__/styleTransformer.js',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/__mocks__/fileMock.js',
+      '<rootDir>/tests/__mocks__/fileMock.js'
   },
   moduleNameMapper: {
     'packages/(.*)$': '<rootDir>/packages/$1',
-    '^src/(.*)$': '<rootDir>/src/$1',
+    '^src/(.*)$': '<rootDir>/src/$1'
   },
   rootDir: path.join(__dirname),
   testMatch: [
     // 匹配测试用例的文件
-    '<rootDir>/tests/**/*.test.js',
+    '<rootDir>/tests/**/*.test.js'
   ],
-  collectCoverageFrom: ['<rootDir>/packages/**/src/*.{js,vue}'],
+  collectCoverageFrom: ['<rootDir>/packages/**/src/*.{js,vue}']
 };
