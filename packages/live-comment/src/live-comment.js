4438a22003f7d470784f15aa6349d316b69be67c
c10342 <1376023740@qq.com>
2021-04-21 19:11:59 +0800

    feat(live-comment.js): 引入image组件样式

--- packages/live-comment/src/live-comment.js
+++ packages/live-comment/src/live-comment.js
@@ -2,6 +2,8 @@ import LiveComment from './live-comment.vue';
 
 import 'packages/live-comment/src/style.scss';
 
+import 'packages/image/src/style.scss';
+
 import 'src/fonts/iconfont.css';
 
 export default LiveComment;
