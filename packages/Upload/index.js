db6e2c7c06d846102bc0d9c9bfde984f9c349e34
c10342 <1376023740@qq.com>
2021-04-22 12:00:36 +0800

    Delete packages/Upload directory

--- packages/Upload/index.js
+++
@@ -1,5 +0,0 @@
-import Upload from './src/upload.js';
-
-Upload.install = (Vue) => Vue.component(Upload.name, Upload);
-
-export default Upload;
--- packages/Upload/src/upload.js
+++
@@ -1,7 +0,0 @@
-import Upload from './upload.vue';
-
-import 'packages/upload/src/style.scss';
-
-import 'packages/button/src/style.scss';
-
-export default Upload;
