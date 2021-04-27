7f6c4b96a53b81f80029f87d2e794029eab45ac9
c10342 <1376023740@qq.com>
2021-04-27 09:23:37 +0800

    test(utils.js): 完善测试工具类

--- tests/utils.js
+++ tests/utils.js
@@ -1,3 +1,30 @@
+// eslint-disable-next-line
+import Vue from 'vue/dist/vue';
+import LinViewUI from 'src/index.js';
+
+Vue.use(LinViewUI);
+
+const createElm = function () {
+  const elm = document.createElement('div');
+
+  document.body.appendChild(elm);
+
+  return elm;
+};
+
+export const createVue = function (Compo) {
+  if (Object.prototype.toString.call(Compo) === '[object String]') {
+    Compo = { template: Compo };
+  }
+  return new Vue(Compo).$mount(createElm());
+};
+
+export const createTest = function (Compo, propsData = {}) {
+  const elm = createElm();
+  const Ctor = Vue.extend(Compo);
+  return new Ctor({ propsData }).$mount(elm);
+};
+
 export const sleep = function (ms = 50) {
   return new Promise((resolve) => setTimeout(() => resolve(), ms));
 };
