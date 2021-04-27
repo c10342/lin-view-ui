5db0fa0c0b0b51c34c200e2309fec28b2c9196f8
c10342 <1376023740@qq.com>
2021-04-27 10:31:26 +0800

    test(form): 添加form组件单元测试用例

--- tests/form/form.test.js
+++ tests/form/form.test.js
@@ -2,6 +2,8 @@ import { shallowMount } from '@vue/test-utils';
 
 import Form from 'packages/form/index.js';
 
+import { createVue } from '../utils.js';
+
 const creatForm = (obj = {}) => {
   return shallowMount(Form, obj);
 };
@@ -28,5 +30,72 @@ describe('插槽', () => {
       }
     });
     expect(wrapper.find('.lin-form').text()).toEqual('你好');
+  });
+});
+
+describe('组合', () => {
+  it('表单校验', async () => {
+    const vm = createVue({
+      template: `
+      <lin-form :model="form1" :rules="rules" ref="loginForm">
+      <lin-form-item labelWidth='20px' ref='formitem' label="用户名" prop="userName">
+        <lin-input v-model="form1.userName" placeholder="请输入用户名" />
+      </lin-form-item>
+      <lin-form-item ref='ageFormItem'>
+        <lin-input v-model="form1.age" placeholder="请输入年龄" />
+      </lin-form-item>
+    </lin-form>
+      `,
+      data() {
+        return {
+          form1: {
+            userName: '',
+            age: ''
+          },
+          rules: {
+            userName: [{ required: true, message: '请输入用户名' }]
+          }
+        };
+      },
+      methods: {
+        validate() {
+          return new Promise((resolve) => {
+            this.$refs.loginForm.validate((valid) => {
+              resolve(valid);
+            });
+          });
+        },
+        resetForm() {
+          this.$refs.loginForm.clearValidate();
+        }
+      }
+    });
+    // 校验表单
+    let result = await vm.validate();
+    const formitem = vm.$refs.formitem;
+    const message = formitem.$el.querySelector('.lin-form-item-errormsg');
+    expect(result).toBe(false);
+    expect(message).toBeTruthy();
+    expect(message.innerHTML).toMatch('请输入用户名');
+    // 清空检验
+    vm.resetForm();
+    expect(formitem.errorMsg).toBe('');
+    // 输入值
+    vm.form1.userName = '张三';
+    // 检验表单
+    result = await vm.validate();
+    expect(result).toBe(true);
+
+    formitem.validate = function () {
+      return new Promise((resolve, reject) => {
+        reject(new Error('false'));
+      });
+    };
+    result = await vm.validate();
+    expect(result).toBe(false);
+
+    const ageFormItem = vm.$refs.ageFormItem;
+    expect(ageFormItem.validate()).toBeFalsy();
+    expect(ageFormItem.itemLabelWidth).toBe(0);
   });
 });
