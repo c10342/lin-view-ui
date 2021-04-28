bf08ce163c5894f7fe212da6675683ece270b78c
c10342 <1376023740@qq.com>
2021-04-28 10:51:45 +0800

    test(utils.js): 完善测试工具类

--- tests/utils.js
+++ tests/utils.js
@@ -13,18 +13,36 @@ const createElm = function () {
 
   return elm;
 };
+/**
+ * 回收vm
+ * @param {*} vm
+ */
+export const destroyVM = function (vm) {
+  vm.$destroy && vm.$destroy();
+  vm.$el && vm.$el.parentNode && vm.$el.parentNode.removeChild(vm.$el);
+};
 
-export const createVue = function (Compo) {
+/**
+ * 创建一个vue实例
+ * @param {*} Compo 组件配置，可直接传template
+ * @returns
+ */
+export const createVue = function (Compo, mounted = false) {
   if (Object.prototype.toString.call(Compo) === '[object String]') {
     Compo = { template: Compo };
   }
-  return new Vue(Compo).$mount(createElm());
+  return new Vue(Compo).$mount(mounted === false ? null : createElm());
 };
 
-export const createTest = function (Compo, propsData = {}) {
-  const elm = createElm();
+/**
+ * 创建一个测试组件实例
+ * @param {*} Compo 组件对象
+ * @param {*} propsData props数据
+ * @returns
+ */
+export const createTest = function (Compo, propsData = {}, mounted = false) {
   const Ctor = Vue.extend(Compo);
-  return new Ctor({ propsData }).$mount(elm);
+  return new Ctor({ propsData }).$mount(mounted === false ? null : createElm());
 };
 
 export const sleep = function (ms = 50) {
