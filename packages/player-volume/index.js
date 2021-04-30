893dbd564ad2056444aeb269be982ca751af176b
c10342 <1376023740@qq.com>
2021-04-30 14:44:11 +0800

    feat(player-volume): 新增player-volume组件

---
+++ packages/player-volume/index.js
@@ -0,0 +1,5 @@
+import PlayerVolume from './src/player-volume.js';
+
+PlayerVolume.install = (Vue) => Vue.component(PlayerVolume.name, PlayerVolume);
+
+export default PlayerVolume;
---
+++ packages/player-volume/src/player-volume.js
@@ -0,0 +1,5 @@
+import PlayerVolume from './player-volume.vue';
+
+import 'packages/player-volume/src/style.scss';
+
+export default PlayerVolume;
