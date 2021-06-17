4408dd358dc5a0cb1a73842e5ab30eb41d09e1c3
c10342 <1376023740@qq.com>
2021-06-17 22:04:25 +0800

    feat(player-fullscreen): 添加player-fullscreen组件

---
+++ packages/player-fullscreen/index.js
@@ -0,0 +1,6 @@
+import PlayerFullscreen from './src/player-fullscreen.vue';
+
+PlayerFullscreen.install = (Vue) =>
+  Vue.component(PlayerFullscreen.name, PlayerFullscreen);
+
+export default PlayerFullscreen;
