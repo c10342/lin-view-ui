23f17d8f87d427553f1f123f238a46cdb197ad54
c10342 <1376023740@qq.com>
2021-06-19 16:23:29 +0800

    style(mixins): 修改导出的变量名称

--- packages/mixins/index.js
+++ packages/mixins/index.js
@@ -1,5 +1,5 @@
-export { default as DateMixins } from "./src/date.js";
-export { default as DocumentClickMixins } from "./src/documentClick.js";
-export { default as DragMixins } from "./src/drag.js";
-export { default as LocaleMixins } from "./src/locale.js";
-export { default as ValidateMixins } from "./src/validate.js";
+export { default as DateMixin } from "./src/date.js";
+export { default as DocumentClickMixin } from "./src/documentClick.js";
+export { default as DragMixin } from "./src/drag.js";
+export { default as LocaleMixin } from "./src/locale.js";
+export { default as ValidateMixin } from "./src/validate.js";
