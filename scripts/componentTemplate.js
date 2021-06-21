781b53d896f496b70a943746b8393fd30d5e5c84
c10342 <1376023740@qq.com>
2021-06-21 19:18:36 +0800

    build(template): 修改生成的组件模板

--- scripts/componentTemplate.js
+++ scripts/componentTemplate.js
@@ -1,11 +1,11 @@
 const chalk = require("chalk");
-const handlebars = require('handlebars')
+const handlebars = require("handlebars");
 const fs = require("fs");
 const path = require("path");
 
 const packageRoot = path.resolve(__dirname, "../packages");
 
-const themeChalkSrc= path.resolve(packageRoot,'./theme-chalk/src')
+const themeChalkSrc = path.resolve(packageRoot, "./theme-chalk/src");
 
 const argv = process.argv;
 
@@ -39,51 +39,86 @@ if (fs.existsSync(compomentPath)) {
 }
 
 // // 创建组件根目录
-fs.mkdirSync(compomentPath)
+fs.mkdirSync(compomentPath);
 // // 创建src目录
-const compomentSrcPath = path.resolve(compomentPath,'src')
-fs.mkdirSync(compomentSrcPath)
+const compomentSrcPath = path.resolve(compomentPath, "src");
+fs.mkdirSync(compomentSrcPath);
 
-function writePakcageTpl(){
-    const parmas = {
-        name:componentName
-    }
-    const tplStr = fs.readFileSync(path.resolve(__dirname,'./template/package.tpl'), 'utf-8')
-    const package = handlebars.compile(tplStr)(parmas)
-    fs.writeFileSync(path.resolve(compomentPath,'package.json'),package)
+function writePakcageTpl() {
+  const parmas = {
+    name: componentName,
+  };
+  const tplStr = fs.readFileSync(
+    path.resolve(__dirname, "./template/package.tpl"),
+    "utf-8"
+  );
+  const package = handlebars.compile(tplStr)(parmas);
+  fs.writeFileSync(path.resolve(compomentPath, "package.json"), package);
 }
 
-function writeIndexTpl(){
-    const parmas = {
-        name:componentName,
-        importName:toHump(componentName)
-    }
-    const tplStr = fs.readFileSync(path.resolve(__dirname,'./template/index.tpl'), 'utf-8')
-    const indexStr = handlebars.compile(tplStr)(parmas)
-    fs.writeFileSync(path.resolve(compomentPath,'index.js'),indexStr)
+function writeIndexTpl() {
+  const parmas = {
+    name: componentName,
+    importName: toHump(componentName),
+  };
+  const tplStr = fs.readFileSync(
+    path.resolve(__dirname, "./template/index.tpl"),
+    "utf-8"
+  );
+  const indexStr = handlebars.compile(tplStr)(parmas);
+  fs.writeFileSync(path.resolve(compomentPath, "index.js"), indexStr);
 }
 
-function writeVueTpl(){
-    const parmas = {
-        compName:`Lin${toHump(componentName)}`
-    }
-    const tplStr = fs.readFileSync(path.resolve(__dirname,'./template/vue.tpl'), 'utf-8')
-    const vueStr = handlebars.compile(tplStr)(parmas)
-    fs.writeFileSync(path.resolve(compomentSrcPath,`${componentName}.vue`),vueStr)
+function writeVueTpl() {
+  const parmas = {
+    compName: `Lin${toHump(componentName)}`,
+  };
+  const tplStr = fs.readFileSync(
+    path.resolve(__dirname, "./template/vue.tpl"),
+    "utf-8"
+  );
+  const vueStr = handlebars.compile(tplStr)(parmas);
+  fs.writeFileSync(
+    path.resolve(compomentSrcPath, `${componentName}.vue`),
+    vueStr
+  );
 }
 
-function writeScssTpl(){
-  const tplStr = fs.readFileSync(path.resolve(__dirname,'./template/scss.tpl'), 'utf-8')
-  fs.writeFileSync(path.resolve(themeChalkSrc,`${componentName}.scss`),tplStr)
-  fs.appendFileSync(path.resolve(themeChalkSrc,`index.scss`),`\n@import './${componentName}.scss';`)
+function writeScssTpl() {
+  const tplStr = fs.readFileSync(
+    path.resolve(__dirname, "./template/scss.tpl"),
+    "utf-8"
+  );
+  fs.writeFileSync(
+    path.resolve(themeChalkSrc, `${componentName}.scss`),
+    tplStr
+  );
+  fs.appendFileSync(
+    path.resolve(themeChalkSrc, `index.scss`),
+    `\n@import './${componentName}.scss';`
+  );
 }
 
-function writeTpl(){
-    writePakcageTpl()
-    writeIndexTpl()
-    writeVueTpl()
-    writeScssTpl()
-    console.log(chalk.blueBright(`${componentName}模板创建成功`));
+function writeReadmeTpl() {
+  const parmas = {
+    name: componentName,
+    componentName: toHump(componentName),
+  };
+  const tplStr = fs.readFileSync(
+    path.resolve(__dirname, "./template/readme.tpl"),
+    "utf-8"
+  );
+  const readmeStr = handlebars.compile(tplStr)(parmas);
+  fs.writeFileSync(path.resolve(compomentPath, "README.md"), readmeStr);
 }
 
-writeTpl()
+function writeTpl() {
+  writePakcageTpl();
+  writeIndexTpl();
+  writeVueTpl();
+  writeScssTpl();
+  writeReadmeTpl()
+  console.log(chalk.blueBright(`${componentName}模板创建成功`));
+}
+
+writeTpl();
--- scripts/template/package.tpl
+++ scripts/template/package.tpl
@@ -7,6 +7,18 @@
     "lin-view-ui",
     "{{name}}"
   ],
+  "homepage": "https://github.com/c10342/lin-view-ui/tree/master/packages/{{name}}",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/c10342/lin-view-ui"
+  },
+  "bugs": {
+    "url": "https://github.com/c10342/lin-view-ui/issues"
+  },
+  "files": [
+    "dist",
+    "src"
+  ],
   "author": "c10342",
   "license": "MIT",
   "peerDependencies": {
