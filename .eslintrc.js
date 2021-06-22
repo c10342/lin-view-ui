bcc72b5db059712929d2396dc60ba44091a4f6fe
c10342 <1376023740@qq.com>
2021-06-22 22:54:18 +0800

    style(.eslint.js): 删除注释代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,10 @@
 module.exports = {
-  // parser: "@babel/eslint-parser",
   env: {
     browser: true,
     es2021: true,
     node: true,
     jest: true
   },
-  // 识别webpack别名
-  // settings: {
-  //   'import/resolver': {
-  //     webpack: {
-  //       config: './build/webpack.base.js'
-  //     }
-  //   }
-  // },
   extends: ["plugin:vue/essential", "standard", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 12,
