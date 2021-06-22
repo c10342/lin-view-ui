5086725f374f71979b57e74843c891bbf3088262
c10342 <1376023740@qq.com>
2021-06-22 17:34:34 +0800

    build(build): 打包文档

---
+++ build/md-loader/config.js
@@ -0,0 +1,61 @@
+const Config = require('markdown-it-chain');
+
+const anchorPlugin = require('markdown-it-anchor');
+
+const taskLists = require('markdown-it-task-lists');
+
+const markdownItTableOfContents = require('markdown-it-table-of-contents');
+
+const containers = require('./containers');
+
+const overWriteFenceRule = require('./fence');
+
+const getPinyin = require('./pinyin');
+
+const config = new Config();
+
+function getLink(link) {
+  let py = getPinyin(decodeURIComponent(link).replace(/\s+/g, ''));
+  py = py[0] ? py[0] : link;
+  return py.toUpperCase().replace(/[^a-zA-Z]/g, '');
+}
+
+config.options
+  .html(true)
+  .linkify(true)
+  .end()
+  .plugin('anchor')
+  .use(anchorPlugin, [
+    {
+      level: 2,
+      permalink: false,
+      // permalinkBefore: true,
+      // permalinkHref: function(link) {
+      //   return "#" + getLink(link);
+      // },
+      slugify: getLink
+    }
+  ])
+  .end()
+  .plugin('toc')
+  .use(markdownItTableOfContents, [
+    {
+      includeLevel: [2],
+      // containerHeaderHtml: `<div class="toc-container-header">Catalog</div>`,
+      transformLink(link) {
+        return `#${getLink(link)}`;
+      }
+    }
+  ])
+  .end()
+  .plugin('containers')
+  .use(containers)
+  .end()
+  .plugin('taskLists')
+  .use(taskLists)
+  .end();
+
+const md = config.toMd();
+overWriteFenceRule(md);
+
+module.exports = md;
---
+++ postcss.config.js
@@ -0,0 +1,4 @@
+// postcss-loader配置文件
+module.exports = {
+  plugins: [require('autoprefixer')]
+};
