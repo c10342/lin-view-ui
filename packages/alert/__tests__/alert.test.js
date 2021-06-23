94e9b0782b0afeb8cfe66e96faa84aa19fc1c8b9
c10342 <1376023740@qq.com>
2021-06-23 23:15:56 +0800

    chore(alert.test.js): 根据eslint格式修复代码

--- packages/alert/__tests__/alert.test.js
+++ packages/alert/__tests__/alert.test.js
@@ -1,75 +1,74 @@
+import { mount } from "@vue/test-utils";
 
-import { mount } from '@vue/test-utils';
+import Alert from "../index.js";
 
-import Alert from '../index.js';
+const title = "this is title";
 
-const title = 'this is title'
+const description = "this is description";
 
-const description = 'this is description'
-
-describe('Alert', () => {
-  test('render test & class', () => {
+describe("Alert", () => {
+  test("render test & class", () => {
     const wrapper = mount(Alert, {
       propsData: {
         title: title
-      },
-    })
-    expect(wrapper.find('.lin-alert-title').text()).toEqual(title)
-    expect(wrapper.find('.lin-alert').classes()).toContain('lin-alert-success')
-  })
+      }
+    });
+    expect(wrapper.find(".lin-alert-title").text()).toEqual(title);
+    expect(wrapper.find(".lin-alert").classes()).toContain("lin-alert-success");
+  });
 
-  test('type', () => {
+  test("type", () => {
     const wrapper = mount(Alert, {
       propsData: {
         title: title,
-        type: 'warning',
-      },
-    })
-    expect(wrapper.find('.lin-alert').classes()).toContain('lin-alert-warning')
-  })
+        type: "warning"
+      }
+    });
+    expect(wrapper.find(".lin-alert").classes()).toContain("lin-alert-warning");
+  });
 
-  test('description', () => {
+  test("description", () => {
     const wrapper = mount(Alert, {
       propsData: {
         title: title,
-        description: description,
-      },
-    })
-    expect(wrapper.find('.lin-alert-description').text()).toEqual(description)
-  })
+        description: description
+      }
+    });
+    expect(wrapper.find(".lin-alert-description").text()).toEqual(description);
+  });
 
-  test('effect', () => {
+  test("effect", () => {
     const wrapper = mount(Alert, {
       propsData: {
-        title:title,
-        effect: 'dark',
-      },
-    })
-    expect(wrapper.find('.lin-alert').classes()).toContain('lin-alert-dark')
-  })
+        title: title,
+        effect: "dark"
+      }
+    });
+    expect(wrapper.find(".lin-alert").classes()).toContain("lin-alert-dark");
+  });
 
-  test('title slot', () => {
+  test("title slot", () => {
     const wrapper = mount(Alert, {
       slots: {
-        title: title,
-      },
-    })
-    expect(wrapper.find('.lin-alert-content').text()).toContain(title)
-  })
+        title: title
+      }
+    });
+    expect(wrapper.find(".lin-alert-content").text()).toContain(title);
+  });
 
-  test('closable', async () => {
+  test("closable", async () => {
     const wrapper = mount(Alert, {
       propsData: {
-        closable: true,
-      },
-    })
+        closable: true
+      }
+    });
 
-    const closeBtn = wrapper.find('.lin-alert-close')
-    expect(closeBtn.exists()).toBe(true)
+    const closeBtn = wrapper.find(".lin-alert-close");
+    expect(closeBtn.exists()).toBe(true);
 
-    await closeBtn.trigger('click')
-    expect(wrapper.vm.show).toBe(false)
-    wrapper.vm.afterLeave()
-    expect(wrapper.emitted().close).toBeTruthy()
-  })
-})
+    await closeBtn.trigger("click");
+    expect(wrapper.vm.show).toBe(false);
+    wrapper.vm.afterLeave();
+    expect(wrapper.emitted().close).toBeTruthy();
+  });
+});
