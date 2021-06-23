4f58264027b3f6226084a67887df66232bdcda6a
c10342 <1376023740@qq.com>
2021-06-23 23:14:14 +0800

    test(jest.config.js): 修改jest配置

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  testEnvironment:'jsdom',
+  testEnvironment: "jsdom",
   moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
+    "js",
+    "jsx",
+    "json",
     // 告诉 Jest 处理 `*.vue` 文件
-    'vue'
+    "vue"
   ],
   transform: {
     // 用 `vue-jest` 处理 `*.vue` 文件
-    '.*\\.(vue)$': 'vue-jest',
-    '^.+\\.jsx?$': 'babel-jest',
+    ".*\\.(vue)$": "vue-jest",
+    "^.+\\.jsx?$": "babel-jest"
     // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
     //   '<rootDir>/tests/__mocks__/fileMock.js'
   },
   moduleNameMapper: {
-    "^@lin-view-ui/(.*)$":"<rootDir>/packages/$1/index.js"
+    "^@lin-view-ui/(.*)$": "<rootDir>/packages/$1/index.js"
   },
   rootDir: path.join(__dirname),
   testMatch: [
     // 匹配测试用例的文件
-    '<rootDir>/packages/**/__tests__/*.test.js'
+    "<rootDir>/packages/**/__tests__/*.test.js"
   ],
   collectCoverageFrom: [
     // '<rootDir>/packages/**/src/*.{js,vue}'
-    '<rootDir>/packages/alert/src/alert.vue',
-    '<rootDir>/packages/backtop/src/backtop.vue',
+    "<rootDir>/packages/alert/src/alert.vue",
+    "<rootDir>/packages/backtop/src/backtop.vue",
     // '<rootDir>/packages/badge/src/badge.vue',
     // '<rootDir>/packages/button/src/button.vue',
     // '<rootDir>/packages/checkbox/src/checkbox.vue',
@@ -38,11 +38,8 @@ module.exports = {
     // '<rootDir>/packages/form-item/src/form-item.vue',
     // '<rootDir>/packages/image/src/image.vue',
     // '<rootDir>/packages/input/src/input.vue',
-    // '<rootDir>/packages/loading/src/construct.js',
-    // '<rootDir>/packages/loading/src/directive.js',
-    // '<rootDir>/packages/loading/src/loading.vue',
-    // '<rootDir>/packages/loading/src/service.js',
+    "<rootDir>/packages/loading/src/*.{js,vue}"
     // '<rootDir>/packages/limit-textarea/src/limit-textarea.vue'
-  ],
+  ]
   // setupFiles: ['<rootDir>/tests/__mocks__/shim.js']
 };
