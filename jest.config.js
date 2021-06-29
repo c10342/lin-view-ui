1c12e0fc10c96568f829cfa9a66d375aa2875e0b
c10342 <1376023740@qq.com>
2021-06-29 11:24:21 +0800

    chore(jest.config.js): 删除注释代码

--- jest.config.js
+++ jest.config.js
@@ -25,21 +25,10 @@ module.exports = {
     "<rootDir>/packages/**/__tests__/*.test.js"
   ],
   collectCoverageFrom: [
-    // '<rootDir>/packages/**/src/*.{js,vue}'
     "<rootDir>/packages/alert/src/alert.vue",
     "<rootDir>/packages/backtop/src/backtop.vue",
-    // '<rootDir>/packages/badge/src/badge.vue',
-    // '<rootDir>/packages/button/src/button.vue',
-    // '<rootDir>/packages/checkbox/src/checkbox.vue',
-    // '<rootDir>/packages/checkbox-group/src/checkbox-group.vue',
-    // '<rootDir>/packages/col/src/col.vue',
-    // '<rootDir>/packages/dialog/src/dialog.vue',
-    // '<rootDir>/packages/form/src/form.vue',
-    // '<rootDir>/packages/form-item/src/form-item.vue',
     "<rootDir>/packages/image/src/image.vue",
-    // '<rootDir>/packages/input/src/input.vue',
     "<rootDir>/packages/loading/src/*.{js,vue}",
-    // '<rootDir>/packages/limit-textarea/src/limit-textarea.vue'
     "<rootDir>/packages/upload/src/upload.vue"
   ]
   // setupFiles: ['<rootDir>/tests/__mocks__/shim.js']
