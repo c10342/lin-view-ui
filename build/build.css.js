8d8334b118b140102589446817e1df384a8d5cc9
c10342 <1376023740@qq.com>
2021-10-25 11:51:34 +0800

    build(build.css.js): 样式打包调整

--- build/build.css.js
+++ build/build.css.js
@@ -1,20 +1,12 @@
-const fs = require("fs");
-
 const { src, dest } = require("gulp");
 
 const sass = require("gulp-sass");
 
 const cssmin = require("gulp-clean-css");
 
-const rename = require("gulp-rename");
-
 const autoprefixer = require("gulp-autoprefixer");
 
 const { resolveRoot } = require("./utils.js");
-
-const path = require("path");
-
-const styleRoot = resolveRoot("./src/theme-chalk");
 
 const copyFont = async () => {
   await src(resolveRoot("./src/theme-chalk/fonts") + "/**")
@@ -23,30 +15,17 @@ const copyFont = async () => {
   console.log("fonts", "done");
 };
 
-const buildScss = async name => {
-  await src(resolveRoot(`./src/theme-chalk/${name}`))
+const buildScss = async () => {
+  await src(resolveRoot(`./src/theme-chalk/*.scss`))
     .pipe(sass().on("error", sass.logError))
     .pipe(autoprefixer({ cascade: false }))
     .pipe(cssmin())
-    .pipe(
-      rename(srcPath => {
-        const basename = name.split(".")[0];
-        srcPath.basename = basename;
-        srcPath.extname = ".css";
-      })
-    )
     .pipe(dest(resolveRoot("./lib/theme-chalk")));
-  console.log(name, "done");
+  console.log("css", "done");
 };
 
 const build = () => {
-  const externalFile = ["iconfont.css", "_var.scss"];
-  const styleList = fs
-    .readdirSync(styleRoot)
-    .filter(fileName => fs.statSync(path.resolve(styleRoot, fileName)).isFile())
-    .filter(fileName => !externalFile.includes(fileName));
-
-  styleList.forEach(fileName => buildScss(fileName));
+  buildScss();
   copyFont();
 };
 
