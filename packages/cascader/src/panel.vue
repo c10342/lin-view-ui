b11f39bfd0478d191c581338c1e4b8f82e2552d1
c10342 <1376023740@qq.com>
2021-12-30 19:19:42 +0800

    feat(cascader):级联选择器

--- packages/cascader/src/panel.vue
+++ packages/cascader/src/panel.vue
@@ -28,7 +28,16 @@
 </template>
 
 <script lang='ts'>
-import { computed, defineComponent, inject, reactive, ref } from "vue";
+import {
+  computed,
+  DefineComponent,
+  defineComponent,
+  inject,
+  nextTick,
+  onBeforeUpdate,
+  reactive,
+  ref
+} from "vue";
 import { componentName } from "./enum";
 import LinCascader from "./cascader.vue";
 export default defineComponent({
@@ -47,10 +56,11 @@ export default defineComponent({
   },
   setup(props) {
     const cascader = inject<typeof LinCascader>("cascader");
+    const childrenRef = ref<DefineComponent[]>([]);
     // children节点列表
     const currentList = ref([]);
     // 当前选中数据源的对象
-    const current = reactive<{[key:string]:any}>({});
+    const proxyData = reactive<{current:{[key:string]:any}}>({current:{}})
     // 是否显示加载器
     const loading = ref(false);
     // 指定选项的唯一值为选项对象的某个属性值
@@ -111,17 +121,16 @@ export default defineComponent({
       return null;
     });
 
-
     // 点击下拉框选项
-    async function onItemClick(data:any) {
+    async function onItemClick(data: any) {
       if (data[disabled.value]) {
         return;
       }
       // 点击的是同一个数据,防止重复渲染
-      if (data[valueKey.value] === current[valueKey.value]) {
+      if (data[valueKey.value] === proxyData.current[valueKey.value]) {
         return;
       }
-      current.value = data;
+      proxyData.current = data;
       if (lazy && lazyLoad && !data[leaf.value]) {
         // 开启了动态加载子节点,并且当前点击的不是最后一个节点
         if (!data[children.value]) {
@@ -131,7 +140,7 @@ export default defineComponent({
           const result = await lazyLoad.value({ level: props.level + 1, data });
           // 找出是属于哪个对象的子节点
           const index = props.options.findIndex(
-            (item:any) => item[valueKey.value] === data[valueKey.value]
+            (item: any) => item[valueKey.value] === data[valueKey.value]
           );
           // 追加子节点
           // eslint-disable-next-line
@@ -142,81 +151,99 @@ export default defineComponent({
       handleBehaver(data);
     }
     // 处理点击行为
-    function handleBehaver(data) {
+    function handleBehaver(data: any) {
       // 清空所有子组件的children列表，因为需要显示新的children列表
-      this.clearList(this.$children);
-      this.currentList = [];
-      if (data[this.children] && data[this.children].length !== 0) {
+      clearList(childrenRef.value);
+      currentList.value = [];
+      if (data[children.value] && data[children.value].length !== 0) {
         // 点击的不是最后有一个子节点，即不是最后一个面板，则需要显示下一个面板出来
-        this.currentList = data[this.children];
+        currentList.value = data[children.value];
       } else {
         // 点击的是最后一项,则隐藏面板
-        this.cascader?.hidePuop();
+        cascader?.hidePuop();
       }
       // 设置选中的值
-      this.cascader?.setValue(data, this.level);
+      cascader?.setValue(data, props.level);
     }
     // 清空所有子组件的children列表,递归
-    function clearList(children) {
+    function clearList(children: DefineComponent[]) {
       children.forEach((child) => {
-        child.currentList = [];
-        if (child.$children.length !== 0) {
-          this.clearList(child.$children);
+        child.currentList.value = [];
+        if (child.childrenRef.value.length !== 0) {
+          clearList(child.childrenRef.value);
         }
       });
     }
     // 是否需要显示右侧箭头图标，最后一个子节点面板不需要显示有测箭头图标
-    function showIconRight(data) {
-      if (this.lazy && this.lazyLoad) {
+    function showIconRight(data: any) {
+      if (lazy.value && lazyLoad.value) {
         // 动态加载则通过标志位判断是否为最后一个子节点面板
-        return !data[this.leaf];
+        return !data[leaf.value];
       }
-      return data[this.children] && data[this.children].length !== 0;
+      return data[children.value] && data[children.value].length !== 0;
     }
     // 动态加载子节点，请求的时候显示loading图标
-    function showLoading(data) {
-      const { valueKey } = this;
-      return data[valueKey] === this.current[valueKey] && this.loading;
+    function showLoading(data: any) {
+      return data[valueKey.value] === proxyData.current[valueKey.value] && loading.value;
     }
-  },
-  created() {
-    // 监听displayPuop事件
-    this.$on("displayPuop", (data) => {
-      if (data && data[this.level]) {
-        const { valueKey } = this;
+
+    function displayPuop(data: any) {
+      if (data && data[props.level]) {
+        // const { valueKey } = this;
         // 找出改层是否有选中的值
-        const index = this.options.findIndex(
-          (item) => item[valueKey] === data[this.level][valueKey]
+        const index = props.options.findIndex(
+          (item: any) =>
+            item[valueKey.value] === data[props.level][valueKey.value]
         );
         if (index > -1) {
           // 改层有选中的值
           // 获取可选数据源当前选中值的对象
-          const currentData = this.options[index];
-          if (currentData[this.disabled]) {
+          const currentData = props.options[index] as [];
+          if (currentData[disabled.value]) {
             return;
           }
           // 获取子节点列表
-          this.currentList = currentData[this.children] || [];
+          currentList.value = currentData[children.value] || [];
           // 当前选中数据源的对象
-          this.current = currentData || {};
-          this.$nextTick(() => {
+          proxyData.current = currentData || {};
+          nextTick(() => {
             // 通知子组件
-            this.$children.forEach((child) => {
+            // this.$children.forEach((child) => {
+            //   if (child.$options.name === componentName.panel) {
+            //     child.$emit("displayPuop", valueArr.value);
+            //   }
+            // });
+            childrenRef.value.forEach((child) => {
               if (child.$options.name === componentName.panel) {
-                child.$emit("displayPuop", this.valueArr);
+                child.$emit("displayPuop", valueArr.value);
               }
             });
           });
         } else {
           // 改层没有选中的值
-          this.currentList = [];
-          this.current = {};
+          currentList.value = [];
+          proxyData.current = {};
         }
       } else {
-        this.currentList = [];
-        this.current = {};
+        currentList.value = [];
+        proxyData.current = {};
       }
+    }
+
+    function getChildrenRef(ref: DefineComponent) {
+      childrenRef.value.push(ref);
+    }
+
+    onBeforeUpdate(() => {
+      childrenRef.value = [];
     });
-  },
+
+    return {
+      // currentList,
+      // valueKey,
+      // label
+      // ...toRefs(proxyData),
+    }
+  }
 });
 </script>
