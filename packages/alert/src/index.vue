73130a04a7158bb984b76cba32ce0f8a1c975a08
c10342 <1376023740@qq.com>
2022-05-10 23:03:20 +0800

    feat(alert): init alert

--- packages/alert/src/index.vue
+++ packages/alert/src/index.vue
@@ -1,10 +1,94 @@
 <template>
-  <div>alert</div>
+  <div
+    :class="[
+      'lin-alert',
+      `lin-alert-${type}`,
+      `lin-alert-${effect}`,
+      { 'lin-alert-center': center }
+    ]"
+    v-if="show"
+  >
+    <slot name="icon">
+      <LinIcon class="lin-alert-icon" :name="icon" v-if="icon" />
+    </slot>
+    <div class="lin-alert-content">
+      <slot name="title">
+        <span class="lin-alert-title">{{ title }}</span>
+      </slot>
+      <slot name="description">
+        <span class="lin-alert-description" v-if="description">
+          {{ description }}
+        </span>
+      </slot>
+    </div>
+    <span class="lin-alert-close" v-if="closable" @click="onCloseClick">
+      <slot name="close">
+        <LinIcon name="close" />
+      </slot>
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import LinIcon from "@packages/icon";
 export default defineComponent({
-  name: "LinAlert"
+  name: "LinAlert",
+  components: { LinIcon },
+  props: {
+    // 标题
+    title: {
+      type: String,
+      default: ""
+    },
+    // 辅助性文字
+    description: {
+      type: String,
+      default: ""
+    },
+    // 图标
+    icon: {
+      type: String,
+      default: ""
+    },
+    // 是否可关闭
+    closable: {
+      type: Boolean,
+      default: false
+    },
+    // 类型
+    type: {
+      type: String,
+      default: "success",
+      options: ["success", "warning", "info", "danger"]
+    },
+    // 主题
+    effect: {
+      type: String,
+      default: "light",
+      options: ["light", "dark"]
+    },
+    // 文字是否居中
+    center: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ["close"],
+  setup(props, context) {
+    // 是否显示
+    const show = ref(true);
+    const onCloseClick = () => {
+      show.value = false;
+    };
+    const afterLeave = () => {
+      context.emit("close");
+    };
+    return {
+      show,
+      onCloseClick,
+      afterLeave
+    };
+  }
 });
 </script>
