492f502c758c1492199ade6e664b1ce7056d4ca4
林家富 <linjiafu@agora.io>
2022-05-13 14:11:52 +0800

    feat(locale): add locale

---
+++ packages/locale/index.ts
@@ -0,0 +1,2 @@
+export { default as zhCN } from "./src/zh-CN";
+export { default as enUS } from "./src/en-US";
---
+++ packages/locale/src/zh-CN.ts
@@ -0,0 +1,95 @@
+export default {
+  LinViewUI: {
+    ShowMore: {
+      showText: "显示更多",
+      hiddenText: "收起"
+    },
+    Dialog: {
+      title: "提示"
+    },
+    Selector: {
+      show: "展开",
+      hide: "收起"
+    },
+    Choice: {
+      placeholder: "请选择",
+      emptyTip: "暂无数据",
+      searchPlaceholder: "请输入"
+    },
+    DatePicker: {
+      year: "年",
+      month: "月",
+      placeholder: "请选择日期",
+      sun: "日",
+      mon: "一",
+      tue: "二",
+      wed: "三",
+      thu: "四",
+      fir: "五",
+      sat: "六"
+    },
+    LimitTextarea: {
+      exceeded: "已超出",
+      enterInput: "还可以输入",
+      word: "个字",
+      entered: "已输入",
+      placeholder: "请输入内容"
+    },
+    Image: {
+      errorMsg: "加载失败"
+    },
+    Pagination: {
+      total: "共",
+      strip: "条",
+      goto: "前往",
+      page: "页"
+    },
+    VideoCard: {
+      imgAlt: "图片",
+      errorTip: "加载失败"
+    },
+    LiveComment: {
+      viewPeople: "人正在观看",
+      see: "查看",
+      publishOpinion: "发表您的观点",
+      login: "登录",
+      commentAfter: "后发表评论",
+      btnText: "发送",
+      placeholder: "请输入",
+      title: "直播评论"
+    },
+    VideoPlayer: {
+      live: "直播",
+      goBack: "快退",
+      fastForward: "快进",
+      second: "秒",
+      speed: "倍速",
+      switch: "已经切换至",
+      quality: "画质",
+      volume: "音量"
+    },
+    PageHeader: {
+      more: "更多",
+      backTip: "返回"
+    },
+    Cascader: {
+      placeholder: "请选择",
+      emptyTip: "暂无数据"
+    },
+    DateAxis: {
+      more: "更多",
+      sun: "星期日",
+      mon: "星期一",
+      tue: "星期二",
+      wed: "星期三",
+      thu: "星期四",
+      fir: "星期五",
+      sat: "星期六"
+    },
+    MessageBox: {
+      cancelButtonText: "取消",
+      confirmButtonText: "确定",
+      inputErrorMessage: "输入的数据不合法!"
+    }
+  }
+};
