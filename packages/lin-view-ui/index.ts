4a2488c38cc9f2cd5d418808b5fbfffd5587d231
林家富 <linjiafu@agora.io>
2022-05-13 14:14:15 +0800

    feat(index): add component

--- packages/lin-view-ui/index.ts
+++ packages/lin-view-ui/index.ts
@@ -1,12 +1,23 @@
+import ConfigProvider from "@packages/config-provider";
 import Button from "@packages/button";
 import Alert from "@packages/alert";
 import Icon from "@packages/icon";
 import Col from "@packages/col";
 import Row from "@packages/row";
-import ConfigProvider from "@packages/config-provider";
+import SelectorGroup from "@packages/selector-group";
+import SelectorItem from "@packages/selector-item";
 import { App } from "vue";
 
-const components = [Button, Alert, ConfigProvider, Icon, Col, Row];
+const components = [
+  Button,
+  Alert,
+  ConfigProvider,
+  Icon,
+  Col,
+  Row,
+  SelectorGroup,
+  SelectorItem
+];
 
 const install = (app: App) => {
   components.forEach((component) => {
