292d263dc28b318ffece8b5e92d38af264a510f7
林家富 <linjiafu@agora.io>
2022-05-17 11:25:00 +0800

    feat(index): add collapse

--- packages/lin-view-ui/index.ts
+++ packages/lin-view-ui/index.ts
@@ -6,6 +6,9 @@ import Col from "@packages/col";
 import Row from "@packages/row";
 import SelectorGroup from "@packages/selector-group";
 import SelectorItem from "@packages/selector-item";
+import CollapseGroup from "@packages/collapse-group";
+import CollapseItem from "@packages/collapse-item";
+import CollapseTransition from "@packages/collapse-transition";
 import { App } from "vue";
 
 const components = [
@@ -16,7 +19,10 @@ const components = [
   Col,
   Row,
   SelectorGroup,
-  SelectorItem
+  SelectorItem,
+  CollapseGroup,
+  CollapseItem,
+  CollapseTransition
 ];
 
 const install = (app: App) => {
