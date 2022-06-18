ffa02715cc2989b5d58e83a680c1cd1762a9eec4
c10342 <1376023740@qq.com>
2022-06-18 23:45:29 +0800

    feat(hooks): add useDrag

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,3 +1,5 @@
 export * from "./src/use-global-config";
 
+export * from "./src/use-drag";
+
 export * from "./src/use-local";
---
+++ packages/hooks/src/use-drag.ts
@@ -0,0 +1,93 @@
+import { isUndefined, parseNumber } from "@packages/utils";
+import { onBeforeUnmount, onMounted, Ref } from "vue";
+
+export const useDrag = (drag = false, dragRef: Ref<HTMLElement | null>) => {
+  // 开始点
+  let startY = 0;
+  let startX = 0;
+
+  // 鼠标按下，开始拖拽
+  const onDragClick = (event: MouseEvent) => {
+    if (!drag) {
+      return;
+    }
+    // 防止选中文字，导致mouseup事件丢失
+    document.body.classList.add("user-select-none");
+    // 开始点
+    startY = event.clientY;
+    startX = event.clientX;
+    document.addEventListener("mousemove", onMousemove);
+    document.addEventListener("mouseup", onMouseup);
+  };
+  const onMousemove = (event: MouseEvent) => {
+    if (!dragRef.value) {
+      return;
+    }
+    // 结束点
+    const endY = event.clientY;
+    const endX = event.clientX;
+    // 移动的偏移量
+    const offsetY = endY - startY;
+    const offsetX = endX - startX;
+    // 需要拖拽容器
+    const rect = dragRef.value.getBoundingClientRect();
+    const left = rect.left;
+    const top = rect.top;
+    const width = rect.width;
+    const height = rect.height;
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    startY = endY;
+    startX = endX;
+    let dialogTop = parseNumber(top + offsetY, 10);
+    let dialogLeft = parseNumber(left + offsetX, 10);
+    // 判断是否越界，超出视图范围
+    dialogTop = dialogTop < 0 ? 0 : dialogTop;
+    dialogLeft = dialogLeft < 0 ? 0 : dialogLeft;
+    dialogTop =
+      dialogTop > windowHeight - height ? windowHeight - height : dialogTop;
+    dialogLeft =
+      dialogLeft > windowWidth - width ? windowWidth - width : dialogLeft;
+    updateStyle({
+      top: `${dialogTop}px`,
+      left: `${dialogLeft}px`,
+      transform: "none",
+      marginTop: "0px",
+      marginBottom: "0px",
+      marginLeft: "0ox",
+      marginRight: "0px"
+    });
+  };
+
+  const updateStyle = (styleObj: Partial<CSSStyleDeclaration>) => {
+    Object.keys(styleObj).forEach((key: any) => {
+      const value = styleObj[key];
+      if (!dragRef.value || isUndefined(value)) {
+        return;
+      }
+      dragRef.value.style[key] = value;
+    });
+  };
+  // 鼠标抬起
+  const onMouseup = () => {
+    document.body.classList.remove("user-select-none");
+    removeListener();
+  };
+  const removeListener = () => {
+    document.removeEventListener("mousemove", onMousemove);
+    document.removeEventListener("mouseup", onMouseup);
+  };
+
+  onMounted(() => {
+    if (dragRef.value) {
+      dragRef.value.addEventListener("mousedown", onDragClick);
+    }
+  });
+
+  onBeforeUnmount(() => {
+    removeListener();
+    if (dragRef.value) {
+      dragRef.value.removeEventListener("mousedown", onDragClick);
+    }
+  });
+};
