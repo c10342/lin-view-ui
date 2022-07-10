c2e94302e8cff1069c14f00d553f6762569e9205
c10342 <1376023740@qq.com>
2022-07-10 16:31:03 +0800

    ci(.eslintrc.js): add defineOptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
+  globals: {
+    defineOptions: true
+  },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
