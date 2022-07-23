5113c1f968203b941add5365452ac254d9ae60c7
c10342 <1376023740@qq.com>
2022-07-23 16:39:06 +0800

    ci(.eslintrc.js): 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,17 @@ module.exports = {
   },
   parser: "vue-eslint-parser",
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended"
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true
+    }
   },
   globals: {
     defineOptions: true
@@ -20,6 +24,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": "off",
-    "@typescript-eslint/ban-types": "off"
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-var-requires": "off"
   }
 };
