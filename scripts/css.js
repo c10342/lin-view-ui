7b8f2ec4609ff212fc5ef5c90c0015b6c09e0386
c10342 <1376023740@qq.com>
2025-01-11 18:03:39 +0800

    update

--- scripts/css.js
+++ scripts/css.js
@@ -3,12 +3,9 @@ const sass = require("gulp-sass")(require("sass-embedded"));
 const cssmin = require("gulp-clean-css");
 const autoprefixer = require("gulp-autoprefixer");
 const path = require("path");
-const del = require("del");
 
 const buildScss = async () => {
   const distPath = path.resolve(__dirname, "../dist/theme-chalk");
-  // 先删除旧的文件
-  await del(distPath, { force: true });
   src(path.resolve(__dirname, "../packages/theme-chalk/**/*.scss"))
     .pipe(sass().on("error", sass.logError))
     .pipe(autoprefixer({ cascade: false }))
